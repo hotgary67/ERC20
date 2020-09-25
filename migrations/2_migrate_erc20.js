49a1d8da1bea99c31a2e71490f76c2c17d245397
hotgary67 <69466703+hotgary67@users.noreply.github.com>
2020-09-25 15:03:41 +0200

    Update 2_migrate_erc20.js

--- migrations/2_migrate_erc20.js
+++ migrations/2_migrate_erc20.js
@@ -2,7 +2,6 @@ const ERC20 = artifacts.require("ERC20");
 
 const _name = "ErEr";
 const _symbol = "ER";
-//const _address = "0x80F53D4fCc0E722CBCFd9c6E465661Da73f7fFC3";
 
 const _amount = "100"
 
@@ -11,14 +10,5 @@ module.exports = function(deployer, network, accounts) {
     instance.mint(accounts[0], _amount)
 
    });
-
-   //}).catch(function(err){
-    //  console.log("error:" +err);
-
-//module.exports = function(deployer, network, accounts) {
-  //deployer.deploy(ERC20, _total_supply, accounts[0]);
-  //  deployer.deploy(ERC20, _name, _symbol);
- // }); 
-
 
 };
