bd273bd822ac0f7b71b87645647d981260389cb1
hotgary67 <69466703+hotgary67@users.noreply.github.com>
2020-09-25 15:03:16 +0200

    Update erc20.js

--- test/erc20.js
+++ test/erc20.js
@@ -1,7 +1,5 @@
 const ERC20 = artifacts.require('ERC20');
 
-
-
 contract('ERC20', function(accounts) {
   it("Should check if token balance is 100 and assert True if correct ", function() {
     var token;
